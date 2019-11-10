4c83db3f1b29bf08cdebf4e86b1d5f7740bf8e5d
Rogerio <blogtecnologiacominformacao@gmail.com>
2019-11-10 11:53:45 -0300

    VT Scan url

---
+++ app/models/vt.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import config
+
+API_KEY_VT = config.API_KEY_VT
+
+def scan_url(urlscan):
+
+    url = 'https://www.virustotal.com/vtapi/v2/url/report'
+    params = {'apikey': API_KEY_VT, 'resource': urlscan}
+    response = requests.get(url, params=params)
+
+    data = json.loads(response.text)
+    print(data)
+
+    if data['response_code'] == 0: # url não encontrada no database do VT
+        resposta = False
+    else:
+        resposta = True if data['positives'] > 0 else False
+    return resposta
